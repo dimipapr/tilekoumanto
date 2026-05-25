2eafb7ad313f722ffd5503a91817f6242f837e6e
Dimitris Papadimitriou <dimipapr@gmail.com>
2026-05-25 22:51:46 +0300

    added api/helth response from django

--- backend/django/devices/views.py
+++ backend/django/devices/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+def health_check(request):
+    # Just return a simple heartbeat. 
+    # If the request reaches here, Django is healthy.
+    return JsonResponse({"status": "OK"})
