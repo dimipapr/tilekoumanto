b55f3930fd718d9cbb0a2980e9e7c048ab0f2461
Dimitris Papadimitriou <dimipapr@gmail.com>
2026-08-08 16:12:42 +0300

    bringup esp32 comms coproccessor

---
+++ device/targets/esp32/main/app_main.c
@@ -0,0 +1,53 @@
+#include "esp_log.h"
+#include "led_strip.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "tilekoumanto";
+
+#define LED_GPIO GPIO_NUM_48
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "ESP32-S3 alive");
+
+    led_strip_handle_t led_strip;
+
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_GPIO,
+        .max_leds = 1,
+        .led_model = LED_MODEL_WS2812,
+        .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB,
+        .flags.invert_out = false,
+    };
+
+    led_strip_rmt_config_t rmt_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT,
+        .resolution_hz = 10 * 1000 * 1000,
+        .mem_block_symbols = 0,
+        .flags.with_dma = false,
+    };
+
+    ESP_ERROR_CHECK(
+        led_strip_new_rmt_device(
+            &strip_config,
+            &rmt_config,
+            &led_strip
+        )
+    );
+
+    while (1) {
+        ESP_LOGI(TAG, "LED ON");
+
+        led_strip_set_pixel(led_strip, 0, 0, 0, 10);
+        led_strip_refresh(led_strip);
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        ESP_LOGI(TAG, "LED OFF");
+
+        led_strip_clear(led_strip);
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
